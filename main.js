2946b118c0b792af9ef5da1987872de6fde3e63b
Надежда Умралиева <nadezhdaumralieva@MacBook-Air-Nadezda-4.local>
2026-01-26 20:20:45 +0300

    Initializing project

---
+++ main.js
@@ -0,0 +1,7 @@
+// main.js
+console.log("Приложение для погоды инициализировано");
+
+document.addEventListener("DOMContentLoaded", () => {
+  const currentWeatherEl = document.getElementById("currentWeather");
+  currentWeatherEl.textContent = "Информация о погоде будет здесь";
+});
